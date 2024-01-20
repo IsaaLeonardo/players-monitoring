f3d991bd7b541be2b35213f9034854e6f90e73a4
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-01-19 23:16:31 -0400

    Creation for PlayersList styles

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+function Player() {
+  return (
+    <div className="bg-white py-10 px-5 shadow-md rounded-xl mb-3">
+      <p className="font-bold mb-1 text-gray-700 uppercase">
+        Nombre: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+
+      <p className="font-bold mb-1 text-gray-700 uppercase">
+        Posición: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+
+      <p className="font-bold mb-1 text-gray-700 uppercase">
+        Fecha de práctica: {''}
+        <span className="font-normal normal-case">19 de enero 2024</span>
+      </p>
+
+      <p className="font-bold mb-1 text-gray-700 uppercase">
+        Oxigenación: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+
+      <p className="font-bold mb-1 text-gray-700 uppercase">
+        Ritmo cardíaco: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+
+      <p className="font-bold mb-1 text-gray-700 uppercase">
+        Síntomas: {''}
+        <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cursus est enim, a sagittis erat mollis non..</span>
+      </p>
+    </div>
+  )
+}
+
+export default Player
--- src/components/PlayersList.jsx
+++ src/components/PlayersList.jsx
@@ -1,7 +1,21 @@
+import Player from "./Player"
+
 function PlayersList() {
   return (
     <div className="md:w-1/2 lg:w-3/5">
-      <h2 className="md:w-3/5 text-center font-black text-3xl">Listado de Jugadores</h2>
+      <h2 className="text-center font-black text-3xl">Listado de Jugadores</h2>
+
+      <p className="text-center text-lg mt-1 mb-3">
+        Administra jugadores y {''}
+        <span className="text-indigo-600 font-bold">visualiza datos</span>
+      </p>
+
+      <div className="px-5">
+        <Player />
+        <Player />
+        <Player />
+        <Player />
+      </div>
     </div>
   )
 }
