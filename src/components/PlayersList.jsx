c2903078460dd004c90b6286232200cad3ac2c22
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-01-22 15:52:51 -0400

    Changing text on list in accordance to players added

--- src/components/PlayersList.jsx
+++ src/components/PlayersList.jsx
@@ -3,12 +3,24 @@ import Player from "./Player"
 function PlayersList({ players }) {
   return (
     <div className="md:w-1/2 lg:w-3/5">
-      <h2 className="text-center font-black text-3xl">Listado de Jugadores</h2>
-
-      <p className="text-center text-lg mt-1 mb-3">
-        Administra jugadores y {''}
-        <span className="text-indigo-600 font-bold">visualiza datos</span>
-      </p>
+      { players.length === 0 ? (
+        <>
+          <h2 className="text-center font-black text-3xl">No hay jugadores registrados</h2>
+          <p className="text-center text-lg mt-1 mb-3">
+            Agrega jugadores y {''}
+            <span className="text-indigo-600 font-bold">registra datos</span>
+          </p>
+        </>
+      ) : (
+        <>
+          <h2 className="text-center font-black text-3xl">Listado de Jugadores</h2>
+          <p className="text-center text-lg mt-1 mb-3">
+            Administra jugadores y {''}
+            <span className="text-indigo-600 font-bold">visualiza datos</span>
+          </p>
+        </>
+      )}
+      
 
       <div className="px-5 md:max-h-[792px] md:overflow-y-scroll">
         {
