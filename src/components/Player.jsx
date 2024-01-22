7a47447611391c3ac07e6415f6a294c5920d6188
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-01-22 14:52:20 -0400

    Show data in player card from list

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,34 +1,36 @@
-function Player() {
+function Player({ player }) {
+  const { name, position, date, oxigenLevels, heartRate, syntoms } = player
+
   return (
     <div className="bg-white py-10 px-5 shadow-md rounded-xl mb-3">
       <p className="font-bold mb-1 text-gray-700 uppercase">
         Nombre: {''}
-        <span className="font-normal normal-case">Hook</span>
+        <span className="font-normal normal-case">{ name }</span>
       </p>
 
       <p className="font-bold mb-1 text-gray-700 uppercase">
         Posición: {''}
-        <span className="font-normal normal-case">Hook</span>
+        <span className="font-normal normal-case">{ position }</span>
       </p>
 
       <p className="font-bold mb-1 text-gray-700 uppercase">
         Fecha de práctica: {''}
-        <span className="font-normal normal-case">19 de enero 2024</span>
+        <span className="font-normal normal-case"> { date } </span>
       </p>
 
       <p className="font-bold mb-1 text-gray-700 uppercase">
         Oxigenación: {''}
-        <span className="font-normal normal-case">Hook</span>
+        <span className="font-normal normal-case"> { oxigenLevels } </span>
       </p>
 
       <p className="font-bold mb-1 text-gray-700 uppercase">
         Ritmo cardíaco: {''}
-        <span className="font-normal normal-case">Hook</span>
+        <span className="font-normal normal-case"> { heartRate } </span>
       </p>
 
       <p className="font-bold mb-1 text-gray-700 uppercase">
         Síntomas: {''}
-        <span className="font-normal normal-case">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc cursus est enim, a sagittis erat mollis non..</span>
+        <span className="font-normal normal-case">{ syntoms }</span>
       </p>
     </div>
   )
