b92dee3fe015fa33b61090f8604565e37d575da7
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2024-01-23 08:52:51 -0400

    Feature to make PlayersList persist data even when reloading

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ import Header from './components/Header'
 import PlayersList from './components/PlayersList'
 
 function App() {
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(() => {
+    const savedPlayers = localStorage.getItem('players');
+    return savedPlayers ? JSON.parse(savedPlayers) : [];
+  });
   const [player, setPlayer] = useState({})
+
+  useEffect(() => {
+    localStorage.setItem('players', JSON.stringify(players));
+  }, [players]);
 
   const handleDelete = id => {
     const newPlayers = players.filter(player => player.id !== id)
